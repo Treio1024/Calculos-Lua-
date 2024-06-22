edbc9838f66d7854793e18f7dcfce98b99d7df4f
Treio1024 <160870426+Treio1024@users.noreply.github.com>
2024-06-21 21:06:19 -0300

    Function_Utils

---
+++ utils.lua
@@ -0,0 +1,12 @@
+local utils = {}
+
+function utils.bask(a, b, c)
+local d
+	d = (b^2 - 4 * a * c)
+	print("O valor de delta e: ", d)
+	print()
+	io.write("O valor de X e: ")
+return (-b + math.sqrt(d)) / (2 * a)
+end
+
+return utils
